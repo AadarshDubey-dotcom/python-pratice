728cce8d8b56983f681152fd2fc0ce654e9ff548
Adarsh Dubey <aadarsh@example.com>
2026-06-17 00:17:12 +0530

    all

---
+++ Basic/DataTypes.py
@@ -0,0 +1,12 @@
+#Data Types 
+#In Python, there are several built-in data types that are used to classify the type of value that a variable can hold. Some of the most common data types include:
+#1. Integer (int): Represents whole numbers without a decimal point.
+#2. Float (float): Represents real numbers with a decimal point.
+#3. String (str): Represents a sequence of characters.
+#4. Boolean (bool): Represents either True or False.
+#5. List: Represents an ordered collection of items.
+#6. Tuple: Represents an ordered, immutable collection of items.
+#7. Dictionary (dict): Represents a collection of key-value pairs.
+#8. Set: Represents an unordered collection of unique items.
+
+#Expression Executive
---
+++ Basic/Variable.py
@@ -0,0 +1,14 @@
+#What is Python ?
+#Python is a high-level, interpreted programming language known for its simplicity and readability. It was created by Guido van Rossum and first released in 1991. Python supports multiple programming paradigms, including procedural, object-oriented, and functional programming. It has a large standard library and a vibrant ecosystem of third-party packages, making it a popular choice for web development, data analysis, artificial intelligence, scientific computing, and more. Python's syntax emphasizes code readability, which allows developers to express concepts in fewer lines of code compared to other programming languages.
+
+#What is a variable in Python?
+#A variable in Python is a named location in memory that is used to store data. It acts as a container for values that can be changed during the execution of a program. In Python, you can create a variable by simply assigning a value to it using the equals sign (=). For example:
+x = 10
+
+#Varibles 
+#In Python, variables are used to store data that can be referenced and manipulated throughout a program. A variable is created when you assign a value to it. The name of the variable can be any valid identifier, which must start with a letter or an underscore (_) and can be followed by letters, digits, or underscores.
+
+name = "Harsh"
+age = 30 
+height = 5.9
+
