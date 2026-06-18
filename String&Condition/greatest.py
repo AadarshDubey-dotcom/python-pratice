3971c8cce3e1be497941f70f8ff036c3300ef88e
Adarsh Dubey <aadarsh@example.com>
2026-06-18 23:58:28 +0530

    commit

---
+++ String&Condition/greatest.py
@@ -0,0 +1,10 @@
+a = int(input("Enter First number  :"))
+b = int(input("Enter Second number  :"))
+c = int(input("Enter Third number "))
+
+if(a > b):
+    print("A is greater")
+elif(b > c):    
+    print("B is greater")
+else:
+    print("C is greater")
