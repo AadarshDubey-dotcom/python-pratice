5ca4c211bbb39b1317e9fa450c08ec2ef45cc21e
Adarsh Dubey <aadarsh@example.com>
2026-06-18 23:49:47 +0530

    Commit

---
+++ String&Condition/Conditional.py
@@ -0,0 +1,10 @@
+light = input("enter the light :")
+
+if(light == "Red"):
+    print("stop")
+elif(light == "Yellow"):
+    print("Look")
+elif(light == "Green"):
+    print("Go")
+else:
+    print("light is broken")            
--- String&Condition/String.py
+++ String&Condition/String.py
@@ -1,2 +1,3 @@
 str = "Adarsh Dubey"
 print(str[2:4])
+print(str[2:4])
