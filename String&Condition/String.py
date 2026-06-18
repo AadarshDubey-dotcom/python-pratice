f5155eac0b02d5600ed7a24bd2aed9837d0f1fe5
Adarsh Dubey <aadarsh@example.com>
2026-06-18 23:43:23 +0530

    commit

--- String&Condition/String.py
+++ String&Condition/String.py
@@ -1,17 +1,2 @@
-#what is String 
-#String is data tytpes that store a sequence of characters
-
-#Basic operator 
-#Concatenation 
-'HELLO' + "WORLD" = "HELLO WORLD"
-
-#Legth of string 
-len(str)
-
-#Indexing 
-#Hello_world
-#012345678910
-
-str = "Hello_world"
-print(str[0])  # outpu will be H
-
+str = "Adarsh Dubey"
+print(str[2:4])
