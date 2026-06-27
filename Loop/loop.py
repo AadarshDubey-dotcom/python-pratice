68c5ef3b1be795630c1c6f0029fda507fa946cc2
Adarsh Dubey <harshpandat014@gmail.com>
2026-06-28 01:13:20 +0530

    commit

---
+++ Loop/loop.py
@@ -0,0 +1,18 @@
+#Write a loop that prints numbers from 1 to 10.
+for i in range(1, 11):
+     print(i)
+
+#Input: N = 5 → Output: 15 (1+2+3+4+5).
+n = int(input("enter the number:"))
+sum = 0
+for i in range(1, n+1):
+     sum += i
+print("Sum =", sum)     
+
+#Print the table of n using a loop.
+n = int(input("Entert the number :"))
+
+for i in range(1,11):
+     print(n*i)
+
+     
