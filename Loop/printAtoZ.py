abc6b10df0d213abe1e14a075221129585da5a77
Adarsh Dubey <harshpandat014@gmail.com>
2026-06-29 01:07:16 +0530

    commit

---
+++ Loop/printAtoZ.py
@@ -0,0 +1,16 @@
+#print A to Z
+
+print("print A to Z", end=" ")
+
+for i in range(65, 91):
+     print(chr(i), end=" ")
+
+# print a to z
+
+print("print a to z", end=" ")
+
+for i in range(65, 91):
+     print(chr(i), end=" ")
+
+
+
