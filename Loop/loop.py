42be8e81d4a6da35e4f217e5a2b0fd60d2620b33
Adarsh Dubey <harshpandat014@gmail.com>
2026-07-04 23:39:32 +0530

    commit

--- Loop/loop.py
+++ Loop/loop.py
@@ -21,3 +21,5 @@ arv = []
 
 for i in range(len(arr)-1, -1,-1):
      print(arr[i])
+
+     
--- String&Condition/String.py
+++ String&Condition/String.py
@@ -1,3 +1,7 @@
 str = "Adarsh Dubey"
 print(str[2:4])
 print(str[2:4])
+
+str = "Harsh Dubey"
+print(str[:2])
+print(str[2:])
