62ed34c9306727c063bedb8b387910c5c7befb7e
Adarsh Dubey <harshpandat014@gmail.com>
2026-07-05 02:00:31 +0530

    commit

---
+++ Function/arthmatic.py
@@ -0,0 +1,20 @@
+#print sum using function in python
+
+def Sum(a,b):
+    return a+b
+print("Sum of two numbers is :", Sum(10,20))
+
+#print subtraction using function in python
+def Subtraction(a,b):
+    return a-b
+print("Subtraction of two numbers is :", Subtraction(20,10))
+
+#print multiplication using function in python
+def Multiplication(a,b):
+    return a*b
+print("Multiplication of two numbers is :", Multiplication(10,20))
+
+#print division using function in python
+def Division(a,b):
+    return a/b
+print("Division of two numbers is :", Division(20,10))
