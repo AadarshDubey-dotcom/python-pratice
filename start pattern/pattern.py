d7566b1219e0bfef922c056048be0fa34b8d2721
Adarsh Dubey <harshpandat014@gmail.com>
2026-07-11 00:35:21 +0530

    commit

--- start pattern/pattern.py
+++ start pattern/pattern.py
@@ -10,3 +10,9 @@ n = 4
 for i in range(1, n+1):
      print(" "*(n-i) + "* "*i)
      
+#Inverted Triangle
+n = 4
+for i in range(n, 0, -1):
+     for j in range(i):
+          print("*", end=" ")
+     print()          
