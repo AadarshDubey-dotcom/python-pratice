dcbf97b68438fe0a693df12d840d56ef6e18c9d3
Adarsh Dubey <harshpandat014@gmail.com>
2026-07-11 00:40:05 +0530

    commit

--- start pattern/pattern.py
+++ start pattern/pattern.py
@@ -23,3 +23,12 @@ for i in range(1, n+1):
      for j in range(1, i+1):
           print(j, end=" ")
      print()                 
+
+#Floyd’s Triangle
+n = 4
+num = 1
+for i in range(1, n+1):
+     for j in range(i):
+          print(num, end=" ")
+          num += 1 
+     print()          
