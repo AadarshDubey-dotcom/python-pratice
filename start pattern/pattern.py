d7bd597b4606327d22de3900fab27e0476e7c225
Adarsh Dubey <harshpandat014@gmail.com>
2026-07-11 00:43:01 +0530

    commit

--- start pattern/pattern.py
+++ start pattern/pattern.py
@@ -32,3 +32,11 @@ for i in range(1, n+1):
           print(num, end=" ")
           num += 1 
      print()          
+
+#Diamond Pattern     
+n = 4
+for i in range(1, n+1):
+     print(" "*(n-i) + "* "*i)
+     
+for i in range(n-1, 0, -1):
+     print(" "*(n-i) + "* "*i)
