92323be6e7519744977c2bdc7318cf437c573e62
Adarsh Dubey <harshpandat014@gmail.com>
2026-07-12 01:10:54 +0530

    commit

--- start pattern/pattern.py
+++ start pattern/pattern.py
@@ -53,3 +53,14 @@ if len(arr) != len(set(arr)):
      print("duplicate is exist")
 else:
      print("duplicate is not exist")
+
+#Prefix Sum
+arr = [1,2,3,4]
+prefix = []
+current_sum = 0
+
+for x in arr:
+     current_sum += x
+     prefix.append(current_sum)
+     
+print(prefix)          
