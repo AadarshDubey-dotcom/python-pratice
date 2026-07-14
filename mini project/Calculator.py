4f24a35a8b69d3f72464caed3bd7b31873bf087a
Adarsh Dubey <harshpandat014@gmail.com>
2026-07-15 01:06:46 +0530

    commit

--- mini project/Calculator.py
+++ mini project/Calculator.py
@@ -1,1 +1,19 @@
 # make calulator in python
+
+#define function 
+def add(a, b):
+     return a + b
+
+def Subtraction(a, b):
+     return a - b
+     
+def Multiplication(a, b):
+     return a * b 
+
+def Division(a, b):
+     if b != 0:
+          return a / b
+     else:
+          return "Error! Division by Zero"
+          
+          
