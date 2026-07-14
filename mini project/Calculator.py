93479683735e9d0a5e91a3a0d49dd5316bd99e9d
Adarsh Dubey <harshpandat014@gmail.com>
2026-07-15 01:59:08 +0530

    commit

--- mini project/Calculator.py
+++ mini project/Calculator.py
@@ -34,3 +34,30 @@ elif choice == "4":
      print("Result :", Division(num1, num2))
 else:
      print("invalid choice")
+
+
+#Project two TODO-APP 
+tasks = []
+
+def add_task(task):
+     tasks.append(task)
+     
+def edit_task(index, new_task):
+     if 0 <= index < len(tasks):
+          tasks[index] = new_task
+     else:
+          print("invalid task index")
+
+def delet_task(index):
+     if 0 <= index < len(tasks):
+          tasks.pop(index)
+     else:
+          print("invalid task index")
+          
+def show_task():
+     if not tasks:
+          print("No tasks yet")
+     else:
+          for i, task in enumerate(tasks):
+               print(f"{i}, {task}")
+          
