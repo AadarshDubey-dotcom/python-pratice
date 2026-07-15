84710b751d34b7371416f222d7cbcfb7665dd4d3
Adarsh Dubey <harshpandat014@gmail.com>
2026-07-16 01:05:30 +0530

    commit

--- mini project/Quiz.py
+++ mini project/Quiz.py
@@ -2,5 +2,18 @@ quiz = {
     "What is the capital of India?" :{
         "options" : ["A. New Delhi", "B. Mumbai", "C. Kolkata", "D. Chennai"],
         "answer" : "A"  
+    },
+    "2 + 2 = ?": {
+         "option" : ["A. 3", "B. 4", "C. 6", "D. 9"],
+         "answer" : "B"
+    },
+    "Which language is used for web development" : {
+         "option" : ["A. Python", "B. C++", "C. JavaScript", "D. Java"],
+         "answer" : "C"
     }
 }
+
+score = 0
+
+print("Welcome to the Quiz Game!\n")
+
