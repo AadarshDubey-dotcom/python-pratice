f87868286b1d786396fff1b7bb9843e712f4f2b6
Adarsh Dubey <harshpandat014@gmail.com>
2026-07-22 01:11:21 +0530

    commit

--- Time and Space complexity/Space.py
+++ Time and Space complexity/Space.py
@@ -1,3 +1,10 @@
 # what is space copmplexity?
 Space Complexity → The measure of memory required by an algorithm until it executes completely.
 
+It depends on:
+
+Input space → Memory used to store input data.
+
+Auxiliary space → Extra memory used during execution (temporary arrays, recursion stack).
+
+Fixed part → Constants, program instructions, compiler overhead.
