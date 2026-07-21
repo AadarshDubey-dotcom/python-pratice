06bd84ad30bd0e0128e9893a8fcff85d1e42742b
Adarsh Dubey <harshpandat014@gmail.com>
2026-07-22 01:12:31 +0530

    commit

--- Time and Space complexity/Space.py
+++ Time and Space complexity/Space.py
@@ -8,3 +8,14 @@ Input space → Memory used to store input data.
 Auxiliary space → Extra memory used during execution (temporary arrays, recursion stack).
 
 Fixed part → Constants, program instructions, compiler overhead."""
+
+#Common Notations
+"""Space complexity is usually expressed in Big‑O notation:
+
+O(1) → Constant space (e.g., accessing an array element).
+
+O(log n) → Logarithmic space (e.g., binary search recursion depth).
+
+O(n) → Linear space (e.g., storing an array of size n).
+
+O(n²) → Quadratic space (e.g., 2D matrix operations)."""
