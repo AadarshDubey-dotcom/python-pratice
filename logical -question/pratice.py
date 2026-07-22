390bca95ba3bab0d1a3d6a8d1bc2d94def9e9dcd
Adarsh Dubey <harshpandat014@gmail.com>
2026-07-23 00:59:35 +0530

    commit

---
+++ logical -question/pratice.py
@@ -0,0 +1,4 @@
+"""Exercise 1. Arithmetic Product and Conditional Logic
+Practice Problem: Write a Python function that accepts two integer numbers.
+If the product of the two numbers is less than or equal to 1000,
+return their product; otherwise, return their sum."""
