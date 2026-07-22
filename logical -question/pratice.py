09feecdd07ef8aa424fa73d9a03bf88ebcfd3494
Adarsh Dubey <harshpandat014@gmail.com>
2026-07-23 01:22:08 +0530

    commit

--- logical -question/pratice.py
+++ logical -question/pratice.py
@@ -2,3 +2,13 @@
 Practice Problem: Write a Python function that accepts two integer numbers.
 If the product of the two numbers is less than or equal to 1000,
 return their product; otherwise, return their sum."""
+
+def check_number(a,b):
+     product = a * b
+     if product <= 1000:
+          return Product
+     else:
+          return a+b
+          
+print(check_number(544,3))
+print(check_number(44,65))
