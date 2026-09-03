72e054f9a9ae2e892e175a40d39e94be7d01c907
Adarsh Dubey <harshpandat014@gmail.com>
2026-09-03 16:43:04 +0530

    commit

--- Pratice problem solving/number_problem.py
+++ Pratice problem solving/number_problem.py
@@ -45,3 +45,16 @@ Final reversed number
 Jab num = 0 ho jata hai, loop rukta hai.
 
 rev ke andar reversed number ready hota hai."""
+
+# Check prime number  
+n = int(input("Enter the number: "))
+
+if n <= 1:
+    print("Not a prime number.")
+else:
+    for i in range(2, n):
+        if n % i == 0:
+            print("Not a prime number.")
+            break
+    else:
+        print("Prime number")
