c805073e3d526a9c06f791437b11ed54c12720dc
Adarsh Dubey <harshpandat014@gmail.com>
2026-09-08 15:08:58 +0530

    commit

--- Pratice problem solving/String_problem.py
+++ Pratice problem solving/String_problem.py
@@ -1,1 +1,10 @@
 #Reverse a string
+word = input("enter the str :")
+
+def reverse_word(word):
+     rev = ""
+     for ch in word:
+          rev = ch + rev
+     return rev     
+     
+print(reverse_word(word))     
