0f81b07132b1240b1972aa41a2d9a056d8ace66c
Adarsh Dubey <harshpandat014@gmail.com>
2026-09-10 21:08:44 +0530

    commit

--- Pratice problem solving/list_and_dic.py
+++ Pratice problem solving/list_and_dic.py
@@ -12,7 +12,22 @@ for n in lis:
 print(rev)     
 
 #Find max and min
+#built in function max() and min()
 lis = [1,2,3,4]
 
 print("Max :", max(lis))
 print("Min :", min(lis))
+
+#Method 2: Loop Logic
+lis = [1,2,3,4]
+
+max_num = lis[0]
+min_num = lis[0]
+
+for n in lis:
+     if n > max_num:
+          max_num = n 
+     if n < min_num:
+          min_num = n 
+print("Max :", max_num)          
+print("Min :", min_num)
