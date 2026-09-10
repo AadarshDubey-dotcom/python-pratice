7968f7d0f8dba56d167d074a2c4df5eb91826ba7
Adarsh Dubey <harshpandat014@gmail.com>
2026-09-10 21:01:40 +0530

    commit

--- Pratice problem solving/list_and_dic.py
+++ Pratice problem solving/list_and_dic.py
@@ -1,1 +1,12 @@
 #Reverse a list  
+#Method 1: Using reverse() (in‑place)
+lis = [1,2,3,4]
+lis.reverse()
+print(lis)
+
+#Method 4: Loop Logic
+lis = [1,2,3,4]
+rev = []
+for n in lis:
+     rev = [n] + rev 
+print(rev)     
