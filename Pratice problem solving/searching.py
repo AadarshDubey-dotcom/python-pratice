4bda572f4416cae4de44a71fe8573773d819e5e5
Adarsh Dubey <harshpandat014@gmail.com>
2026-09-17 13:29:16 +0530

    commit

--- Pratice problem solving/searching.py
+++ Pratice problem solving/searching.py
@@ -14,3 +14,14 @@ if result != -1:
      print(f"Element {search_element} found at index {result}")
 else:
      print(f"Element {search_element} not found")
+
+"""Logic Flow
+Input list → ek list jisme elements stored hain.
+
+Target element → jis element ko search karna hai.
+
+Loop through list → har element ko check karo.
+
+Condition check → agar element target ke equal hai → found.
+
+Return result → index ya message print karo."""     
