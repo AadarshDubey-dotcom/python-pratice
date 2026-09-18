50dbe09ed56ff52de889dd032927884d4eada19a
Adarsh Dubey <harshpandat014@gmail.com>
2026-09-18 21:25:59 +0530

    commit

--- Pratice problem solving/sorting.py
+++ Pratice problem solving/sorting.py
@@ -9,3 +9,16 @@ num = [12,3,4,55,64,2]
 
 num.sort(reverse=True)
 print(num)
+
+# Bubble sort implementation  
+def bubble_sort(arr):
+     for i in range(len(arr)):
+          for j in range(0, len(arr)-i-1):
+               if arr[j] > arr[j+1]:
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+     return arr
+
+number = [65,23,55,76,43,77,87]     
+sorted_list = bubble_sort(number)
+
+print("Sorted List :", sorted_list)
