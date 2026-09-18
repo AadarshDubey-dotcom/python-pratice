aeed9eb01eccc5c6edbd36e9b9d4b0f53d7ee341
Adarsh Dubey <harshpandat014@gmail.com>
2026-09-18 21:43:23 +0530

    commit

--- Pratice problem solving/sorting.py
+++ Pratice problem solving/sorting.py
@@ -32,3 +32,18 @@ Inner loop → har adjacent pair compare karega.
 Swap elements → agar left element bada hai toh dono ko swap karo.
 
 Repeat until sorted → jab tak list ascending order me na aa jaye."""
+
+#Selection sort  
+def selection_sort(arr):
+     for i in range(len(arr)):
+          min_idx = i
+          for j in range(i+1, len(arr)):
+               if arr[j] < arr[min_idx]:
+                    min_idx = j 
+          arr[i], arr[min_idx] = arr[min_idx], arr[i]          
+     return arr
+
+number = [23,43,21,5,6,77]     
+result = selection_sort(number)
+
+print("sorted list :", result)
