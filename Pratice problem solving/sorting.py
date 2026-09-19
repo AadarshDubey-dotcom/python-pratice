f7e430dc541db90d015269c0f0ab4221dc11e7ee
Adarsh Dubey <harshpandat014@gmail.com>
2026-09-19 20:45:50 +0530

    commit

--- Pratice problem solving/sorting.py
+++ Pratice problem solving/sorting.py
@@ -57,3 +57,20 @@ Find minimum → unsorted part me sabse chhota element dhoondo.
 Swap with current → us minimum ko current position ke saath swap karo.
 
 Repeat until sorted → jab tak poori list sorted na ho jaaye."""
+
+# Insertion Sort 
+def insertion_sort(arr):
+     for i in range(1, len(arr)):
+          key = arr[i]
+          j = i-1
+          while j >= 0 and arr[j] > key:
+               arr[j+1] = arr[j]
+               j-=1
+               
+          arr[j+1] = key
+     return arr
+     
+number = [23,43,2,3,44,5]     
+result = insertion_sort(number)
+
+print("Result :", result)
