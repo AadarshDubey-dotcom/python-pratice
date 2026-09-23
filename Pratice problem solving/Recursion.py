4ba135613b00982bac87e08d1e56f7139bb55388
Adarsh Dubey <harshpandat014@gmail.com>
2026-09-23 15:07:48 +0530

    commit

--- Pratice problem solving/Recursion.py
+++ Pratice problem solving/Recursion.py
@@ -1,5 +1,5 @@
  #  Factoria 
- n = int(input("enter the number:"))
+n = int(input("enter the number:"))
 
 def fact(n):
      if n ==0:
@@ -7,3 +7,15 @@ def fact(n):
      return n * fact(n-1)     
 
 print(fact(n))     
+
+#print_number
+n = int(input("enter the number:"))
+
+def print_number(n):
+     if n == 1:
+          print(1, end=" ")
+          return
+     print_number(n-1)
+     print(n, end=" ")
+     
+print_number(n)     
