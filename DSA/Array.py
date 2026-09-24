079c71014425951b7227d78cee53b5e50272b9d8
Adarsh Dubey <harshpandat014@gmail.com>
2026-09-24 14:56:34 +0530

    commit

--- DSA/Array.py
+++ DSA/Array.py
@@ -1,2 +1,14 @@
 #Contains Duplicate
 #Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+class Solution:
+    def hasDuplicate(self, nums):
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
+
+# Object create karo
+solution = Solution()
+print(solution.hasDuplicate([1,2,3,4]))  
