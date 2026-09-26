27507e4df3dce3dd8870a7ffc7c003578a5b66f7
Adarsh Dubey <harshpandat014@gmail.com>
2026-09-26 15:36:46 +0530

    commit

--- OOPs/Abtraction.py
+++ OOPs/Abtraction.py
@@ -18,3 +18,5 @@ b = Bike()
 
 print(c.start())
 print(b.start())
+
+#Abstraction is a process of hiding the implementation details and showing only functionality to the user. In Python, abstraction can be achieved by using abstract classes and methods. An abstract class is a class that cannot be instantiated and is meant to be subclassed. It can contain abstract methods, which are methods that are declared but contain no implementation. Subclasses of the abstract class must provide implementations for the abstract methods.
